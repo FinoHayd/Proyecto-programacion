3ced6e8e826ed419f0fabfb017d7aea4dc4ba122
Hayder Fino <162834041+FinoHayd@users.noreply.github.com>
2025-05-10 09:32:45 -0500

    Update test_conexion.php

--- test_conexion.php
+++ test_conexion.php
@@ -4,20 +4,17 @@ $DB_USER = 'root';
 $DB_PASS = '';
 $DB_NAME = 'petcontrol';
 
-// 1) Habilitar que mysqli lance excepciones en errores
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
 try {
-    // 2) Intentar la conexión
     $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+    
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
     
     echo "¡Conexión exitosa a la base de datos!";
     
-    // 3) Cerrar la conexión
     $conn->close();
-    
-} catch (mysqli_sql_exception $e) {
-    // 4) Capturar errores de conexión u otros errores MySQLi
-    die("Error de conexión: " . $e->getMessage());
+} catch (Exception $e) {
+    die("Error: " . $e->getMessage());
 }
-?>
+?> 
